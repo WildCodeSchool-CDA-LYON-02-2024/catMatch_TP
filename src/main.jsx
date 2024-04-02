6b9e2999d165a78bc24f31ba4ac23af47888a5fb
techmefr <compigni.gaetan@gmail.com>
2024-04-02 17:19:55 +0200

    change rooting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import CatMatch from "./components/CatMatch.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/vote" element={<CatMatch />} />
+    </Routes>
+  </BrowserRouter>
+);
