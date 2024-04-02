07d6ebf49b1928407373b456e96d6803e7e99c54
Attar Hamed <attar30pro@gmail.com>
2024-04-02 16:36:25 +0200

    catmatch app

---
+++ catmatch/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CatMatch from './components/CatMatch';
+
+function App() {
+  return (
+    <div className="App">
+      <CatMatch />
+    </div>
+  );
+}
+
+export default App;
