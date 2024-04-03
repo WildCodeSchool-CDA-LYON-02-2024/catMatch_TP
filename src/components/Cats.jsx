8aa2079357c8cfdef76fbf3b890700ec073fc0d6
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-04-03 17:31:26 +0200

    score done

--- src/components/Cats.jsx
+++ src/components/Cats.jsx
@@ -1,39 +1,39 @@
+// react
 import { useState, useEffect } from "react";
+
+// proptypes
 import PropTypes from "prop-types";
+
+// CSS
 import "./Cats.css";
 
-function Cats({ cats }) {
+function Cats({ cats, scores, updateScores }) {
   const [selectedCats, setSelectedCats] = useState([]);
 
-  // Fonction pour sélectionner aléatoirement deux chats
   const selectRandomCats = () => {
-    let newSelectedCats;
-    while (
-      !newSelectedCats ||
-      JSON.stringify(newSelectedCats) === JSON.stringify(selectedCats)
-    ) {
-      const shuffledCats = cats.sort(() => Math.random() - 0.5);
-      newSelectedCats = shuffledCats.slice(0, 2);
-    }
+    const shuffledCats = cats.sort(() => Math.random() - 0.5); // tri aléatoire -> création d'une nouvelle liste
+    const newSelectedCats = shuffledCats.slice(0, 2); // les deux premiers éléments de la liste
     setSelectedCats(newSelectedCats);
   };
 
   // Appeler la fonction pour sélectionner aléatoirement les chats au chargement initial
   useEffect(() => {
-    selectRandomCats();
-  }, []);
+    selectRandomCats(); // appel de la fonction
+  }, []); // exécution lors du montage du composant
 
-  // Gestionnaire d'événements pour changer les chats lorsqu'on clique sur une image
-  const handleCatClick = () => {
-    selectRandomCats();
+  // MAJ du score + nouveaux chats
+  const handleCatClick = (selectedCat) => {
+    updateScores(selectedCat.name); // score incrémenté quand on clique sur l'image du chat
+    selectRandomCats(); // nouvelle fournée de deux chats aléatoirement
   };
 
   return (
     <>
       {selectedCats.map((cat, index) => (
-        <div key={index} onClick={handleCatClick}>
+        <div key={index} onClick={() => handleCatClick(cat)}>
           <img src={cat.link} alt={cat.name} />
           <p>{cat.name}</p>
+          <p>Score: {scores[cat.name] || 0}</p>
         </div>
       ))}
     </>
@@ -47,6 +47,8 @@ Cats.propTypes = {
       link: PropTypes.string.isRequired,
     })
   ).isRequired,
+  scores: PropTypes.object.isRequired,
+  updateScores: PropTypes.func.isRequired,
 };
 
 export default Cats;
